15461d86da14a05e747789d3816d104bc809d666
Casper <casper.bjorkman@gmail.com>
2023-11-09 11:36:05 +0100

    Added inlupp 3 sources

---
+++ src/test/java/StandardTests.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StandardTests {
+    @Test
+    void subrtract() {
+        assertEquals(1, 1);
+    }
+}
