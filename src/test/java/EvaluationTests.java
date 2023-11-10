0f08355f667760173167e1549a4aa34dbe0589d8
Casper <casper.bjorkman@gmail.com>
2023-11-10 13:47:51 +0100

    Migrated the old manuall tests marking the failing ones disabled.

---
+++ src/test/java/EvaluationTests.java
@@ -0,0 +1,90 @@
+import org.ioopm.calculator.Tests;
+import org.ioopm.calculator.ast.*;
+import org.ioopm.calculator.parser.Environment;
+import org.ioopm.calculator.parser.IllegalExpressionException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class EvaluationTests {
+
+    Environment vars;
+
+    @BeforeEach
+    void setVars() {
+        vars = new Environment();
+    }
+
+
+    @Test
+    void evaluatesToConstant() throws IllegalExpressionException {
+        Assertions.assertEquals(new Constant(15.0), new Subtraction(
+                new Multiplication(
+                        new Addition(
+                                new Constant(1),
+                                new Constant(1)),
+                        new Constant(8)),
+                new Constant(1)).eval(vars));
+        Assertions.assertEquals(new Constant(4.0), new Multiplication(
+                new Addition(new Constant(1), new Constant(1)),
+                new Addition(new Constant(1), new Constant(1))
+        ).eval(vars));
+    }
+
+    @Disabled("Binary helper is miss implemented")
+    @Test
+    void notFullyEvaluated() throws IllegalExpressionException {
+        Assertions.assertEquals(new Variable("x"), new Variable("x").eval(vars));
+
+        SymbolicExpression complex = new Subtraction(
+                new Multiplication(
+                        new Addition(
+                                new Constant(10),
+                                new Constant(2)),
+                        new Constant(2)),
+                new Variable("x"));
+
+        Assertions.assertEquals(new Subtraction(
+                        new Constant(24),
+                        new Variable("x")),
+                complex.eval(vars));
+    }
+
+    @Test
+    void simpleAssignment() throws IllegalExpressionException {
+        SymbolicExpression assignment = new Assignment(
+                new Addition(new Constant(40), new Constant(2)), new Variable("x"));
+
+        Assertions.assertEquals(new Constant(42), assignment.eval(vars));
+        Assertions.assertEquals(new Constant(42), new Variable("x").eval(vars));
+    }
+
+    @Test
+    void nestedAssignment() throws IllegalExpressionException {
+
+        Assertions.assertEquals(new Constant(3),
+                new Assignment(
+                        new Assignment(new Constant(3),
+                                new Variable("x")),
+                        new Variable("y")).eval(vars));
+
+        Assertions.assertEquals(new Constant(3), new Variable("x").eval(vars));
+        Assertions.assertEquals(new Constant(3), new Variable("y").eval(vars));
+    }
+
+    @Test
+    void namedConstantNotReassignable() {
+        SymbolicExpression assignment = new Assignment(
+                new Constant(20),
+                new NamedConstant("Answer", 42));
+
+        Assertions.assertThrowsExactly(
+                IllegalExpressionException.class,
+                () -> assignment.eval(vars));
+
+        Tests.check(vars.isEmpty(), "Vars not updated after exception");
+
+        Tests.testEvaluating(new Variable("Answer"), new Variable("Answer"));
+    }
+}
